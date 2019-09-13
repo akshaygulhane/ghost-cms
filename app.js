83287eadaf398c8be5da5aea22bad2026f1b0fe5
Akshay Gulhane <akshay.gulhane@opcito.com>
2019-09-13 22:50:21 +0530

    add GET API

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
 
 const CONTENT_API_KEY = "c63b5a3e40e018c9c91c513cbe";
 const ADMIN_API_KEY = "5d7b633ae68d124c5fc569e3:c4d3fa4fca8d13d4bb37e21b2ce4677a4551d6fda4265d5609bd0ae35c7a02df";
 const API_URL = "http://localhost:2368";
 
-// curl -H "Authorization: Ghost $token" ?https://{admin_domain}/ghost/api/{version}/admin/{resource}/
+const [id, secret] = ADMIN_API_KEY.split(':');
 
-axios.post(`${API_URL}/ghost/api/v2/admin`,
-    {
-        headers: {
-            'Authorization': 'Ghost ' + ADMIN_API_KEY
+// Create the token (including decoding secret)
+const token = jwt.sign({}, Buffer.from(secret, 'hex'), {
+    keyid: id,
+    algorithm: 'HS256',
+    expiresIn: '5m',
+    audience: `/v2/admin/`
+});
+
+const url = 'http://localhost:2368/ghost/api/v2/admin/posts/';
+const headers = { Authorization: `Ghost ${token}` };
+const payload = { posts: [{ title: 'Hello World' }] };
+
+axios.get(url, { headers })
+    .then(response => {
+        
+        if (response.data.posts.length > 0) {
+            const updated_at = response.data.posts[0].updated_at;
+            const mobiledoc = JSON.parse(response.data.posts[0].mobiledoc);
+            const content = mobiledoc.sections;
+            const length = content.length;
+// console.log(content)
+            // console.log(JSON.stringify(content[0][2]));
+           for(let i = 0; i < length; i++) {
+               console.log(content[i]);
+           }
+
+            const payload = { posts: [{ title: 'Leads', "updated_at": updated_at }] };
+
+            axios.put('http://localhost:2368/ghost/api/v2/admin/posts/5d7b676de68d124c5fc569e4/',
+                payload,
+                {
+                    headers
+                })
+                .then(response => {
+                    console.log(response.status)
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+
+
         }
     })
-    .then(response => {
-        console.log(response.status)
-    })
-    .catch(err => {
-        console.error(err);
-    })
+    .catch(error => {
+        console.error(error)
+    });
